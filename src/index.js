882ce4fff9a922584d4e2b953eb5984ad3a7902b
dorottyat <dorottya.torok.97@gmail.com>
2022-07-14 20:00:31 +0200

    implemented fetch for getting the recipes, creating a recipe and a way of showing the recipes

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// server=localhost:8080
+// client=localhost:5500
+
+let nameInput = document.querySelector("#nameInput");
+let servingInput = document.querySelector("#servingInput");
+let timeInput = document.querySelector("#timeInput");
+let noteInput = document.querySelector("#noteInput");
+let createRecipeBtn = document.querySelector("#createRecipeBtn");
+let recipesDiv = document.querySelector("#recipesDiv");
+
+createRecipeBtn.addEventListener("click", postRecipe);
+
+function postRecipe() {
+  fetch("http://localhost:8080/recipes", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: nameInput.value,
+      serving: servingInput.value,
+      time: timeInput.value,
+      ingredients: ["tojas", "kenyer"],
+      directions: ["fozd meg a tojast", "edd meg a asdasdasd"],
+      note: noteInput.value,
+    }),
+  });
+}
+
+fetch("http://localhost:8080/recipes")
+  .then((res) => res.json())
+  .then((data) => showRecipes(data));
+
+function showRecipes(data) {
+  data.forEach((recipe) => {
+    recipesDiv.innerHTML += showRecipe(recipe);
+  });
+}
+
+function showRecipe(recipe) {
+  return `
+    <div class="recipe">
+      <h2 class="recipe-name">${recipe.name}</h2>
+      <p class="recipe-serving">${recipe.serving}</p>
+      <p class="recipe-time">${recipe.time}</p>
+      <p class="recipe-note">${recipe.note}</p>
+      <ul class="recipe-ingredients"></ul>
+      <ol class="recipe-directions"></ol>
+    </div>
+    `;
+}
